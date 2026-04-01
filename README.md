ee26c07bc5daaf6af07c6f22314dc6657cfe5bf3
Samkit-Jain11 <jainsamkit1105@gmail.com>
2026-04-01 22:45:40 +0530

    day 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Learn_Python
+1 ) print(" ")  this will print something 
+
+@ ) a = input("") # this will take input from user , here 'a' is a variable 
---
+++ printname.py
@@ -0,0 +1,14 @@
+'''print('Hello World 1 \nHello World 2')
+
+print("samkit"+' '+'jain')
+
+x = 'samkit' # here x is a variable printnamee
+y = 'jain'
+z = ' '
+print(x +z + y)
+
+
+print(f"{x} {y}")
+'''
+
+print(f"The entered target ip is " +  input("Enter your target ip ? "))
